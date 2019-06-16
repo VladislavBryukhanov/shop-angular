a4a6376baa7f15117ad8006221b6d403d3c4ef36
nameless <gfedcba625@gmail.com>
2019-06-16 19:44:20 +0300

    implemented sign_in and sign_up material form and validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatIconModule,
+  MatCardModule,
+} from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SignComponent } from './shared/sign/sign.component';
 
 const routes: Routes = [
-  { path: '', component: SignComponent }
+  { path: '', component: SignComponent },
+  { path: 'sign_up', component: SignComponent, data: { signUp: true } }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignComponent
+    SignComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    FormsModule,
+    MatCardModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/shared/sign/sign.component.ts
+++ src/app/shared/sign/sign.component.ts
@@ -1,15 +1,133 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ErrorStateMatcher } from '@angular/material';
+import moment from 'moment';
 
 @Component({
   selector: 'app-sign',
   templateUrl: './sign.component.html',
   styleUrls: ['./sign.component.scss']
 })
-export class SignComponent implements OnInit {
+export class SignComponent implements OnInit, ErrorStateMatcher {
 
-  constructor() { }
+  public hidePassword = true;
+  public signUp: boolean;
+
+  public email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  public password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(32)
+  ]);
+
+  public firstName = new FormControl('', [
+    Validators.required,
+    Validators.minLength(1),
+    Validators.maxLength(20)
+  ]);
+  public lastName = new FormControl('', [
+    Validators.required,
+    Validators.minLength(1),
+    Validators.maxLength(20)
+  ]);
+  public gender = new FormControl(null);
+  public birthday = new FormControl(null);
+
+  public address = new FormControl('', [
+    Validators.required,
+    Validators.minLength(4),
+    Validators.maxLength(64)
+  ]);
+  public phone = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[0-9]{4,12}$'),
+  ]);
+
+  public userForm = new FormGroup({
+    email: this.email,
+    password: this.password,
+    firstName: this.firstName,
+    lastName: this.lastName,
+    gender: this.gender,
+    birthday: this.birthday,
+
+    contactInfo: new FormGroup({
+      address: this.address,
+      phone: this.phone
+    })
+  });
+
+  public genderOptions = [
+    {
+      name: 'Male',
+      value: true,
+    },
+    {
+      name: 'Female',
+      value: false,
+    }
+  ];
+
+  private validation = {
+    email: () => {
+      if (this.email.hasError('email')) {
+        return 'Please enter a valid email address';
+      }
+      if (this.email.hasError('required')) {
+        return 'Email is required';
+      }
+    },
+    password: () => this.validateTextField(this.password, 'Password', 8, 32),
+    firstName: () => this.validateTextField(this.firstName, 'First name', 1, 20),
+    lastName: () => this.validateTextField(this.lastName, 'Last name', 1, 20),
+    address: () => this.validateTextField(this.address, 'Address', 4, 64),
+    phone: () => {
+      if (this.phone.hasError('pattern')) {
+        return 'Wrong phone number format!';
+      }
+      if (this.email.hasError('required')) {
+        return 'Phone number is required';
+      }
+    }
+  };
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route
+      .data
+      .subscribe(({ signUp }) => {
+        this.signUp = signUp;
+      });
   }
 
+  isErrorState(control: FormControl | any, form: FormGroupDirective | NgForm | any): boolean {
+    return control.invalid && (control.dirty || control.touched || form.submitted);
+  }
+
+  validateTextField(fc: FormControl, name: string, minLen: number, maxLen: number) {
+      if (fc.hasError('required')) {
+        return `${name} is required`;
+      }
+      if (fc.hasError('minlength')) {
+        return `${name} must be longer then ${minLen - 1} characters`;
+      }
+      if (fc.hasError('maxlength')) {
+        return `${name} must be less then ${maxLen + 1} characters`;
+      }
+  }
+
+  onSignIn() {
+    console.log(this.userForm.valid);
+  }
+
+  onSignUp() {
+    const birthDay = moment(this.userForm.value.birthday).unix();
+    console.log(this.userForm.valid);
+    console.log(this.userForm);
+  }
 }
